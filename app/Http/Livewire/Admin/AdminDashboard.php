228ad8de721b662923716df74a7cc7e6248effa5
osmanforhad <osmanforhad93@gmail.com>
2021-07-12 18:52:29 +0600

    workng with display ecomerce summery in Admin Dashboard

--- app/Http/Livewire/Admin/AdminDashboard.php
+++ app/Http/Livewire/Admin/AdminDashboard.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Order;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class AdminDashboard extends Component
 {
     public function render()
     {
-        return view('livewire.admin.admin-dashboard')->layout('layouts.base');
+        //fetch order by newest
+        $orders = Order::orderBy('created_at', 'DESC')->get()->take(10);
+
+        //define total sales by deliver status
+        $totalSales = Order::where('status', 'delivered')->count();
+
+        //count total reveneue by deliver status count
+        $totalRevneue = Order::where('status', 'delivered')->sum('total');
+
+        //define daily sales by deliver status and date function
+        $todaySales = Order::where('status', 'delivered')->whereDate('created_at', Carbon::today())->count();
+
+        //count dailyreveneue by deliver status count and with date function
+        $todayRevneue = Order::where('status', 'delivered')->whereDate('created_at', Carbon::today())->sum('total');
+
+        return view('livewire.admin.admin-dashboard', [
+            'orders' => $orders,
+            'totalSales' => $totalSales,
+            'totalRevneue' => $totalRevneue,
+            'todaySales' => $todaySales,
+            'todayRevneue' => $todayRevneue,
+        ])
+            ->layout('layouts.base');
     }
 }
