37b4cd1bf953c32c3767dc72c0f1c32404f0a2b7
osmanforhad <osmanforhad93@gmail.com>
2021-07-12 19:24:49 +0600

    making header functional on login and register page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-base-layout>
     <main id="main" class="main-site left-sidebar">
 
 		<div class="container">
@@ -47,4 +47,4 @@
 		</div><!--end container-->
 
 	</main>
-</x-guest-layout>
+</x-base-layout>
